cff3dcd154763f2a2e03d77678be2610ea812f85
zhangzhong <zhangzhong@yongche.com>
2017-03-23 10:46:31 +0800

    a

--- php_fpm/mod_php_fpm.c
+++ php_fpm/mod_php_fpm.c
@@ -19,54 +19,54 @@
 
 #include "tsar.h"
 
-#define STATS_TEST_SIZE (sizeof(struct stats_php-fpm))
+#define STATS_TEST_SIZE (sizeof(struct stats_php_fpm))
 
-static const char *php-fpm_usage = "    --php-fpm               php-fpm information";
+static const char *php_fpm_usage = "    --php_fpm               php_fpm information";
 
 /*
  * temp structure for collection infomation.
  */
-struct stats_php-fpm {
+struct stats_php_fpm {
     unsigned long long    value_1;
     unsigned long long    value_2;
     unsigned long long    value_3;
 };
 
 /* Structure for tsar */
-static struct mod_info php-fpm_info[] = {
+static struct mod_info php_fpm_info[] = {
     {"value1", SUMMARY_BIT,  0,  STATS_NULL},
     {"value2", DETAIL_BIT,  0,  STATS_NULL},
     {"value3", DETAIL_BIT,  0,  STATS_NULL}
 };
 
 static void
-read_php-fpm_stats(struct module *mod, const char *parameter)
+read_php_fpm_stats(struct module *mod, const char *parameter)
 {
     /* parameter actually equals to mod->parameter */
     char               buf[256];
-    struct stats_php-fpm  st_php-fpm;
+    struct stats_php_fpm  st_php_fpm;
 
     memset(buf, 0, sizeof(buf));
-    memset(&st_php-fpm, 0, sizeof(struct stats_php-fpm));
+    memset(&st_php_fpm, 0, sizeof(struct stats_php_fpm));
 
-    st_php-fpm.value_1 = 1;
-    st_php-fpm.value_2 = 1;
-    st_php-fpm.value_3 = 1;
+    st_php_fpm.value_1 = 1;
+    st_php_fpm.value_2 = 1;
+    st_php_fpm.value_3 = 1;
 
     int pos = sprintf(buf, "%llu,%llu,%llu",
             /* the store order is not same as read procedure */
-            st_php-fpm.value_1,
-            st_php-fpm.value_2,
-            st_php-fpm.value_3);
+            st_php_fpm.value_1,
+            st_php_fpm.value_2,
+            st_php_fpm.value_3);
 
     buf[pos] = '\0';
-    /* send data to tsar you can get it by pre_array&cur_array at set_php-fpm_record */
+    /* send data to tsar you can get it by pre_array&cur_array at set_php_fpm_record */
     set_mod_record(mod, buf);
     return;
 }
 
 static void
-set_php-fpm_record(struct module *mod, double st_array[],
+set_php_fpm_record(struct module *mod, double st_array[],
     U_64 pre_array[], U_64 cur_array[], int inter)
 {
     int i;
@@ -80,5 +80,5 @@ set_php-fpm_record(struct module *mod, double st_array[],
 void
 mod_register(struct module *mod)
 {
-    register_mod_fields(mod, "--php-fpm", php-fpm_usage, php-fpm_info, 3, read_php-fpm_stats, set_php-fpm_record);
+    register_mod_fields(mod, "--php_fpm", php_fpm_usage, php_fpm_info, 3, read_php_fpm_stats, set_php_fpm_record);
 }
